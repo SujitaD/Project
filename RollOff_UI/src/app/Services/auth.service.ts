34849f31ca4b05e4672dda5f91e33d81aa9e5905
SDABEKAR <SDABEKAR@LIN25006369>
2023-04-26 16:12:04 +0530

    changes

---
+++ RollOff_UI/src/app/Services/auth.service.ts
@@ -0,0 +1,62 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import{JwtHelperService} from '@auth0/angular-jwt';
+import { BehaviorSubject } from 'rxjs';
+import { environment } from 'src/environments/environment.development';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+ 
+  currentuser:BehaviorSubject<any>=new BehaviorSubject("null");
+  baseServerUrl=environment.baseApiUrl+"api/"
+  jwtHelperService=new JwtHelperService();
+  constructor(private http:HttpClient) { }
+
+  registerUser(reg:any){
+    //reg:Array<String>
+    return this.http.post(this.baseServerUrl+"User/CreateUser",reg,
+    // {
+    //   Firstname:reg[0],
+    //   Lastname:reg[1],
+    //   Email:reg[2],
+    //   Password:reg[3]
+    // },
+    {
+      responseType:'text',
+    });
+  } 
+  
+  loginUser(reg: any){
+     return this.http.post(this.baseServerUrl+"User/LoginUser",reg,
+    //  {
+      
+    //      Email: reg[0],
+    //      PWD: reg[1]
+    // },
+      {responseType:'text',}
+    );
+ }
+
+setToken(token:string){
+
+  localStorage.setItem("access_token",token);
+  
+   this.loadCurrentUser();
+  
+  }
+  
+  
+  
+  
+   loadCurrentUser(){
+  
+   const Token=localStorage.getItem("Access_token");
+  
+   const userInfo=Token!=null?this.jwtHelperService.decodeToken(Token):null;
+  
+   console.log(userInfo);
+  
+   }
+}
